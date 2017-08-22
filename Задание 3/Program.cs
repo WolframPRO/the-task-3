0dc7c63cd6f2e6f8dec112ad82499c4b28c6275b
Владимир Петров <vmpetrov@edu.hse.ru>
2017-08-22 21:45:24 +0500

    Сделал программу

--- Задание 3/Program.cs
+++ Задание 3/Program.cs
@@ -10,6 +10,31 @@ namespace Задание_3
     {
         static void Main(string[] args)
         {
+
+                double x, y, u;
+
+                Console.WriteLine("Введите координаты X и Y через Enter:");
+
+                bool ok;
+
+                do { ok = Double.TryParse(Console.ReadLine(), out x); if (!ok) Console.WriteLine("Ошибка ввода координаты X!"); } while (!ok);
+                do { ok = Double.TryParse(Console.ReadLine(), out y); if (!ok) Console.WriteLine("Ошибка ввода координаты Y!"); } while (!ok);
+
+                if ((x * x + y * y) > 1 || y > x / 2)
+                {
+                    u = -3;
+                }
+                else
+                {
+                    u = y * y;
+                }
+
+                Console.WriteLine("Результат: " + u);
+
+                Console.ReadKey();
+
         }
+
     }
+
 }
